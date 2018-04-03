83fd0befc788d8b89e1dd77e029dd66ced1117df
nathan <grimaudathan@gmail.com>
2018-04-03 14:48:57 +0200

    feat(created project + map)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+// vue-material
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+// vue-openlayers
+import '../node_modules/openlayers/css/ol.css';
+import olMap from 'vue-openlayers/src/ol-map.vue';
+import olMarker from 'vue-openlayers/src/ol-marker.vue';
+import olBalloon from 'vue-openlayers/src/ol-balloon.vue';
 
 Vue.config.productionTip = false;
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+
+const VueOpenlayers = {
+  install: function(Vue, options) {
+    // wiring project components
+    Vue.component('ol-map', olMap);
+    Vue.component('ol-marker', olMarker);
+    Vue.component('ol-balloon', olBalloon);
+  }
+};
+
+Vue.use(VueOpenlayers);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Map from '@/components/Map';
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
+    Map
   }
 };
 </script>
